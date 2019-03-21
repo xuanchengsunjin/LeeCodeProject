f825a20d213456b062b43c8578ec5e67a0660ab0
XcJimsun <1543510543@qq.com>
2019-03-21 20:28:16 +0800

    数组1.0

--- java/com/jimsun/Array/PrimaryAlgorithm.java
+++ java/com/jimsun/Array/PrimaryAlgorithm.java
@@ -1,5 +1,177 @@
 package com.jimsun.Array;
 
-public class PrimaryAlgorithm {
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PrimaryAlgorithm {
+	/*
+ 	给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。 
+    不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+    返回新的长度。
+    空间复杂度：O(1)
+	*/
+	public int removeDuplicates(int[] nums) {
+		int index=0;
+		for(int i=1;i<nums.length;i++) {
+			if(nums[i] != nums[index]) {
+				nums[++index]=nums[i];
+			}
+		}
+		return ++index;
+	}
+	/*
+	 * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+	 * 输入: [1,2,3,4,5,6,7] 和 k = 3
+	 * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右旋转 1 步: [7,1,2,3,4,5,6]
+     * 向右旋转 2 步: [6,7,1,2,3,4,5]
+     * 向右旋转 3 步: [5,6,7,1,2,3,4]
+     * 空间复杂度：O(1)
+	 * */
+	 public void rotate(int[] nums, int k) {
+		 int length = nums.length;
+		 while(k-->0) { 
+			 int j=length-1;
+			 int temp = nums[j];
+			 while(j>0) {
+				 nums[j]=nums[--j];
+			 }
+			 nums[0]=temp;
+		 }
+	 }
+	 /*
+	  *给定一个整数数组，判断是否存在重复元素。
+      *如果任何值在数组中出现至少两次，函数返回 true。
+      *如果数组中每个元素都不相同，则返回 false。 */ 
+	  public boolean containsDuplicate(int[] nums) {
+		   if(nums.length ==0)
+               return false;
+		    Set<Integer> set = new TreeSet<Integer>();
+		    set.add(nums[0]);
+		    for(int i=1;i<nums.length;i++) {
+		    	if(!set.add(nums[i])) {
+		    		return true;
+		    	}    	
+		    }
+		    return false;
+	  }
+	  /*
+	   *给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+       *最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+       *你可以假设除了整数 0 之外，这个整数不会以零开头。
+       *输入: [1,2,3]
+       *输出: [1,2,4]
+       *解释: 输入数组表示数字 123。*/
+	  public int[] plusOne(int[] digits) {
+	        int length = digits.length;
+	        int add=1;
+	        for(int i=length-1;i>=0;i--) {
+	        	int temp=digits[i]+add;
+	        	add = temp/10;
+	        	digits[i]=temp%10;
+	        	if(add>0) {
+	        		continue;
+	        	}
+	        	break;
+	        }
+	        
+	        if(add>0) {
+	        	int[] a = new int[length+1];
+	        	a[0]= add;
+	        	for(int i=0;i<length;i++) {
+	        		a[i+1] = digits[i];
+	        	}
+	        	digits=a;
+	        }
+	        return digits;
+	  }
+	  /*
+	   * 移动零
+	   * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+	   * */
+	   public void moveZeroes(int[] nums) {
+	       int index=0;
+	       for(int i=0;i<nums.length;i++) {
+	    	   if(nums[i] != 0) {
+	    		   nums[index++] = nums[i];
+	    	   }
+	       }
+	       for(int j=index;j<nums.length;j++) {
+	    	   nums[j]=0;
+	       }        
+	   }
+	   /*
+	    * 两数之和
+	    * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+	    * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+	    * 给定 nums = [2, 7, 11, 15], target = 9
+	    * 因为 nums[0] + nums[1] = 2 + 7 = 9
+	    * 所以返回 [0, 1]
+	    * */
+	    public int[] twoSum(int[] nums, int target) {
+	    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    	map.put(target - nums[0], 0);
+	    	for(int i=1;i<nums.length;i++) {
+	    		if(map.containsKey(nums[i])) {
+	    			return new int[] {map.get(nums[i]),i};
+	    		}
+	    		map.put(target - nums[i], i);
+	    	}
+	        return null;  
+	    }
+	    public int[] twoSumImprove(int[] nums, int target) {
+	    	int[] arr = new int[2];
+	    	int length = nums.length;
+	    	//拷贝数组
+ 	    	int[] copyArr = new int[length];
+ 	    	System.arraycopy(nums,0, copyArr, 0 , length);
+ 	    	//排序
+ 	    	Arrays.sort(nums);
+ 	    	//i和j同时寻址
+	    	for(int i=0,j=length-1;i != j;) {
+	    		if(nums[i]+nums[j] == target) {
+	    			int fromNumber = nums[i];
+	    			int toNumber = nums[j];
+	    			for(int a=0,flag = 0; a<length ;a++) {
+	    	    		if(copyArr[a] == fromNumber && flag !=1) {
+	    	    			arr[0] = a;
+	    	    			if(flag != 0)
+	    	    			   return arr;
+	    	    		    flag = 1;
+	    	    		}else if(copyArr[a] == toNumber && flag !=2) {
+	    	    			arr[1] = a;
+	    	    			if(flag != 0)
+	    	    			   return arr;
+	    	    		    flag = 2;
+	    	    		}
+	    	    	}
+	    			break;
+	    		}else if(nums[i]+nums[j] > target) {
+	    			j--;
+	    		}else {
+	    			i++;
+	    		}
+	    	}  	
+	        return null;  
+	    }
+	    public int[] twoSumImprovePlus(int[] nums, int target) {
+	    	int size = 2048;
+	    	int[] map = new int[size];
+	    	int length = 2047;
+	    	int index;
+	    	for (int i = 0; i < nums.length; i++) {
+				index = nums[i]&length; 
+				if (map[index] != 0) {
+					return new int[] { map[index] - 1, i };
+				} else {
+					map[(target - index)&length ] = i + 1;
+				}
+			}
+			throw new IllegalArgumentException("No two sum solution");
+	    }
 }
--- src/test/java/com/jimsun/Array/TestArray.java
+++ src/test/java/com/jimsun/Array/TestArray.java
@@ -5,7 +5,16 @@ import org.junit.Test;
 public class TestArray {
 	@Test
 	public void testSampleOne() {
+		int[] arr = {0,9,0,4,9};
 		PrimaryAlgorithm a = new PrimaryAlgorithm();
 		
+		printArray(a.twoSumImprovePlus(arr,13));
+	}
+	void printArray(int[] arr) {
+		System.out.println("打印数组如下：");
+		for(int data:arr) {
+			System.out.println(data);
+		}
+		System.out.println("打印结束");
 	}
 }

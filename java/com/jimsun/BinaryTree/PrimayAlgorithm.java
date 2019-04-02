6fb28fb22e8d4e6d235f9f77ea9a6c3a17a7464e
XcJimsun <1543510543@qq.com>
2019-04-02 18:13:35 +0800

    添加二叉树路径和

--- java/com/jimsun/BinaryTree/PrimayAlgorithm.java
+++ java/com/jimsun/BinaryTree/PrimayAlgorithm.java
@@ -167,4 +167,15 @@ public class PrimayAlgorithm {
 	      node.right = build_tree(nums, middle+1, end);
 	      return node;
 	  }
+	  /*路径和
+	   *给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+	   *说明: 叶子节点是指没有子节点的节点。
+	   * */
+	  public boolean hasPathSum(TreeNode root, int sum) {
+		  if(root == null)
+			  return false;
+	      if(root.val == sum && root.right == null && root.left == null) 
+	          return true; 
+	      return hasPathSum(root.left,sum-root.val) || hasPathSum(root.right,sum-root.val);
+	    }
 }
